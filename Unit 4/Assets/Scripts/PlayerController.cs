420e7444ae626eba1b1daa337f05ce0bbcf0659a
dmclaughlin104 <dmclaughlin104@gmail.com>
2023-10-19 21:41:54 +0100

    Commit at 4.2.1

---
+++ Unit 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //variables
+    private Rigidbody playerRB;
+    public float playerSpeed;
+    private GameObject focalPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRB.AddForce(focalPoint.transform.forward * playerSpeed *  forwardInput);
+        
+    }
+}
