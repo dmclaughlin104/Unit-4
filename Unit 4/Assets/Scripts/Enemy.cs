afeac1b556b8d58197cffff54dd3afed7a0f1561
dmclaughlin104 <dmclaughlin104@gmail.com>
2023-10-22 21:41:36 +0100

    Finished 4.4
    Sunday nights

--- Unit 4/Assets/Scripts/Enemy.cs
+++ Unit 4/Assets/Scripts/Enemy.cs
@@ -15,15 +15,23 @@ public class Enemy : MonoBehaviour
     {
         enemyRB = GetComponent<Rigidbody>();//assigning Rigidbody component
         player = GameObject.Find("Player");//assigning Player to variable
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //determining direction toward player
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
-
-
+        //moving enemy toward player
         enemyRB.AddForce(lookDirection * speed);
+
+
+        //destroy enemy if they fall off platform
+        if (transform.position.y < -20.0f)
+        {
+            Destroy(gameObject);
+        }
         
     }
 }
--- Unit 4/Assets/Scripts/SpawnManager.cs
+++ Unit 4/Assets/Scripts/SpawnManager.cs
@@ -6,20 +6,40 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    private int nextWave = 1;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
-
+        SpawnEnemyWave(nextWave);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //setting enemy count - N.B. using the scripts applied, not the tag
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        //if all enemies are defeated, spawn more
+        if (enemyCount == 0)
+        {
+            nextWave++;
+            SpawnEnemyWave(nextWave);
+        }
         
     }
+
+
+    void SpawnEnemyWave(int pWaveNumber)
+    {
+        for (int count = 0; count < pWaveNumber; count++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
+        }
+    }
+
 
 
     //method to generate a new random spawn position
